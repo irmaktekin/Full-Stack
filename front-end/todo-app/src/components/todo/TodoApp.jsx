44a0200231a908515b7c02fd4e00d7eb168b6dd3
irmaktekin <irmaktekin97@gmail.com>
2023-07-22 00:05:27 +0300

    Protecting list todos page with authenticated route.

--- front-end/todo-app/src/components/todo/TodoApp.jsx
+++ front-end/todo-app/src/components/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
 import './TodoApp.css';
 import LogoutComponent from './LogoutComponent';
 import HeaderComponent from './HeaderComponent';
@@ -6,7 +6,21 @@ import ListTodosComponent from './ListTodosComponent';
 import ErrorComponent from './ErrorComponent';
 import WelcomeComponent from './WelcomeComponent';
 import LoginComponent from './LoginComponent';
-import AuthProvider from './security/AuthContext';
+import AuthProvider, { useAuth } from './security/AuthContext';
+
+function AuthenticatedRoute({children}){
+    const authContext = useAuth()
+    if (authContext.isAuthenticated){
+        return children
+
+
+    }
+    else{
+        return <Navigate to="/" />
+    }
+
+}
+
 export default function ToDoApp(){
     return (
         <div className='TodoApp'>
@@ -16,11 +30,12 @@ export default function ToDoApp(){
             <Routes>
                 <Route path='/' element={<LoginComponent/>}/>
                 <Route path='/login' element={<LoginComponent/>}/>
-                <Route path='/welcome/:username' element={<WelcomeComponent/>}/>
-                <Route path='/todos' element={<ListTodosComponent/>}/>
-                <Route path='/logout' element={<LogoutComponent/>}/>
                 <Route path='*' element={<ErrorComponent/>}/>
+                <Route path='/todos' element={<AuthenticatedRoute><ListTodosComponent/></AuthenticatedRoute>}/>
+                    <Route path='/logout' element={<AuthenticatedRoute><LogoutComponent/></AuthenticatedRoute>}/>
 
+                    <Route path='/welcome/:username' element={<AuthenticatedRoute><WelcomeComponent/></AuthenticatedRoute>}/>
+                   
 
 
             </Routes>
