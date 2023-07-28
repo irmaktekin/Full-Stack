1f99550990151912a9ebba700dee8fc6c793e840
irmaktekin <irmaktekin97@gmail.com>
2023-07-28 18:40:51 +0300

    Configuring Auto Generation ofA Swagger Documentation

--- restfulwebservices/src/main/java/com/irmaktekin/restfulwebservices/exception/ResponseEntityExceptionHandler.java
+++ restfulwebservices/src/main/java/com/irmaktekin/restfulwebservices/exception/ResponseEntityExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.irmaktekin.restfulwebservices.user.UserNotFoundException;
-
+ 
 @ControllerAdvice
 public class ResponseEntityExceptionHandler {
 	
